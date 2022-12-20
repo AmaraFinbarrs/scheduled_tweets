f205975093ad0987ac20ef2bc4e3042ed5c5bcd7
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2022-12-20 03:56:47 +0000

    Create a sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get '/about-us', to: 'about#index', as: :about
   get '/home', to: 'main#index'
+  get '/sign_up', to: 'registrations#new'
+  post '/sign_up', to: 'registration#create'
 
 
 end
