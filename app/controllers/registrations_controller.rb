723b82fb28b1e584dc6308f5314ad4963ca5d562
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2022-12-22 09:38:52 +0000

    Handle sign up errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,20 @@ class RegistrationsController < ApplicationController
     end
 
     def create
-        render "created"
+        #render plain: params[:user]
+        #render "created"
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path, notice: "Successfully created account"
+        else
+            flash[:alert] = "Something was wrong"
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
     end
 end
