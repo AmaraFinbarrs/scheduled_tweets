06dfd99ee34375fe14cf1a43437ba82cf2ffe21c
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2022-12-22 10:31:39 +0000

    Create login with session cookies

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,10 +4,9 @@ class RegistrationsController < ApplicationController
     end
 
     def create
-        #render plain: params[:user]
-        #render "created"
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to root_path, notice: "Successfully created account"
         else
             flash[:alert] = "Something was wrong"
